4fd7874af5f6c8bdbf4305ab95317b960a92817f
Juan Torres <juanto121@hotmail.com>
2014-08-05 20:14:51 -0500

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+	app  = express(),
+	server = require('http').createServer(app),
+	io 	= require('socket.io').listen(server);
+
+server.listen(5000,function(){
+	console.log('Listening on port %d', server.address().port);
+});
+
+app.use(express.static( __dirname +'/public'));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  
+  socket.on('message', function(msg){
+  	
+  		console.log('message: '+ msg);
+  		io.emit('message', msg);
+  });
+
+  socket.on('fn',function(msg){
+  	
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+app.get('/',function(req,res){
+	res.sendfile( __dirname + '/index.html');
+});
---
+++ public/js/index_c.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+
+$('button').click(function(e){
+	var idClicked = e.target.id;
+	socket.emit('message', $('#'+idClicked).attr("value"));
+});
+
+$('form').submit(function(){
+	socket.emit('message', $('#m').val());
+	$('#m').val('');
+	return false;
+});
+
+socket.on('message', function(msg){
+	//$('#messages').html.slideUp();
+	$('#messages').html($('<ul class="display" id="messages">').text(msg));
+});
