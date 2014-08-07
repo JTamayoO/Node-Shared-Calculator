fe24ec114ad2aef4146a32d562f249a9ccb698c6
Juan Torres <juanto121@hotmail.com>
2014-08-07 15:15:47 -0500

    basic binary functions added

--- app.js
+++ app.js
@@ -12,17 +12,79 @@ app.get('/',function(req,res){
   res.sendfile( __dirname + '/index.html');
 });
 
+
+
+function EstadoCalc (display, expression)
+{
+  this.display = display;
+  this.expression = expression;
+  this.operando = [];
+  this.operador = [];
+}
+var functions = 
+{
+  prod:'*',
+  plus:'+',
+  min:'-',
+  div:'/'
+};
+estado = new EstadoCalc( '' ,'' );
+
 io.on('connection', function(socket){
-  console.log('a user connected');
-  
+  console.log('a user connected state: ' + estado );
+  io.emit('number', estado.display);
+  io.emit('expression',estado.expression);
+
+  socket.on('number', function(number){
+    estado.display += number;
+    io.emit('number', estado.display);
+  });
+
   socket.on('message', function(msg){
-  	
-  		console.log('message: '+ msg);
+  	  estado.display ++;
+  		console.log('message: '+ msg + 'estadoCalc: ' + estado.display );
   		io.emit('message', msg);
   });
 
-  socket.on('fn',function(msg){
-  	
+
+  //socket.on('unitfn',function())
+  
+  socket.on('binfn',function(msg){
+    var result = 0;
+    
+     if(estado.operando.length === 0){
+        estado.operando[0] = msg;
+        estado.operador[0] = Number(estado.display);
+        estado.display = ''; // No seguir concatenando
+        io.emit('number','');
+        estado.expression +=' '+ estado.operador[0] +' '+ functions[estado.operando[0]];
+     }else{
+        
+        estado.operador[1] = Number(estado.display);
+        switch(estado.operando[0]){
+          case "plus":
+            result = estado.operador[0] + estado.operador[1]; 
+           break;
+          case "min":
+             result = estado.operador[0] - estado.operador[1]; 
+          break;
+          case "prod":
+             result = estado.operador[0] * estado.operador[1]; 
+          break;
+          case "div":
+              result = estado.operador[0] / estado.operador[1]; 
+          break;
+        }
+        console.log(msg);
+        estado.operador[0] = result;
+        estado.operador[1] = null;
+        estado.operando[0] = msg;
+        estado.expression +=' '+ estado.display +' '+ functions[estado.operando[0]];
+        estado.display = '';
+        io.emit('number',result);
+     }
+     
+     io.emit('expression', estado.expression);
   });
 
   socket.on('disconnect', function(){
--- public/js/index_c.js
+++ public/js/index_c.js
@@ -1,9 +1,20 @@
 var socket = io();
 
-
 $('button').click(function(e){
 	var idClicked = e.target.id;
-	socket.emit('message', $('#'+idClicked).attr("value"));
+	var clickedVal =  $('#'+idClicked).attr("value");	
+	//socket.emit('message',$('#messages').text().trim() + clickedVal);
+	if (!isNaN(clickedVal)) {
+		socket.emit( 'number', clickedVal );
+	}else{
+		switch(idClicked){
+			case "btn_eq":
+			break;
+			default:
+				socket.emit('binfn', clickedVal);
+			break;
+		}
+	}
 });
 
 $('form').submit(function(){
@@ -12,6 +23,10 @@ $('form').submit(function(){
 	return false;
 });
 
-socket.on('message', function(msg){
+socket.on('number', function(msg){
 	$('#messages').html($('<ul class="display" id="messages">').text(msg));
+});
+
+socket.on('expression', function(exp){
+	$('#expression').html($('<ul class="expression">').text(exp));
 });
